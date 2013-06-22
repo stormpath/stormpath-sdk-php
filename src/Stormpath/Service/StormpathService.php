c59f5aea5ec530b2179c3d7d76ee83e9f56e47f4
vinayganesh <vinayganeshh@gmail.com>
2013-06-22 16:13:10 -0700

    added the BaseURI and the create client function

--- src/Stormpath/Service/StormpathService.php
+++ src/Stormpath/Service/StormpathService.php
@@ -2,3 +2,19 @@
 
 namespace Stormpath\Service;
 
+use Stormpath\Client\ApiKey;
+
+
+
+final class StormpathService
+{
+
+    const BASEURI = 'https://api.stormpath.com/v1';
+
+    public static function createClient($accessId, $secretKey)
+    {
+        ApiKey::getAccessId($accessId);
+        ApiKey::setSecretKey($secretKey);
+    }
+
+}
