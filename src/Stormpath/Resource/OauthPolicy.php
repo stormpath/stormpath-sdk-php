1439c93938533818a50f1820e0a0f4a217b3e19a
Brian Retterer <brian@stormpath.com>
2015-10-26 18:55:51 -0400

    Doc Blocks for OauthPolicy

--- src/Stormpath/Resource/OauthPolicy.php
+++ src/Stormpath/Resource/OauthPolicy.php
@@ -18,8 +18,14 @@ namespace Stormpath\Resource;
  * limitations under the License.
  */
 
+use Stormpath\Client;
 use Stormpath\Stormpath;
 
+/**
+ * Class OauthPolicy
+ * @package Stormpath\Resource
+ * @since 1.11.0.beta
+ */
 class OauthPolicy extends InstanceResource implements Saveable
 {
     const ACCESS_TOKEN_TTL              = "accessTokenTtl";
@@ -29,31 +35,51 @@ class OauthPolicy extends InstanceResource implements Saveable
     const PATH                          = "oAuthPolicies";
 
 
+    /**
+     * @param $href
+     * @param array $options
+     * @return mixed
+     */
     public static function get($href, array $options = array())
     {
         return Client::get($href, Stormpath::OAUTH_POLICY, self::PATH, $options);
     }
 
+    /**
+     * @return string
+     */
     public function getAccessTokenTtl()
     {
         return $this->getProperty(self::ACCESS_TOKEN_TTL);
     }
 
+    /**
+     * @param $ttl
+     */
     public function setAccessTokenTtl($ttl)
     {
         $this->setProperty(self::ACCESS_TOKEN_TTL, $ttl);
     }
 
+    /**
+     * @return string
+     */
     public function getRefreshTokenTtl()
     {
         return $this->getProperty(self::REFRESH_TOKEN_TTL);
     }
 
+    /**
+     * @param $ttl
+     */
     public function setRefreshTokenTtl($ttl)
     {
         $this->setProperty(self::REFRESH_TOKEN_TTL, $ttl);
     }
 
+    /**
+     * @return null
+     */
     public function getTokenEndpoint()
     {
         return $this->getProperty(self::TOKEN_ENDPOINT);
