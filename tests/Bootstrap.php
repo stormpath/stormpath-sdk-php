a8cc936d9ddd685529083491598571ca91fa7c13
Brian Retterer <brian@stormpath.com>
2015-10-29 15:27:45 -0400

    Make Stormpath PHP SDK PSR-4 Compliant.  Fixes #17

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL | E_STRICT);
 
 function makeUniqueName($provided = null)
 {
-    $name = 'PHP_' . str_replace('.', '_', phpversion()) . '_';
+    $name = 'PHP_' . str_replace('.', '', phpversion()) . '_';
     $name .= str_replace(' ', '_', microtime()) . '_';
     $name .= str_replace(' ', '_', $provided);
 
@@ -17,17 +17,5 @@ if (!file_exists(dirname(__DIR__) . '/composer.lock')) {
         . "See http://getcomposer.org for help with installing composer\n");
 }
 
-$root = realpath(dirname(dirname(__FILE__)));
-$library = "$root/src";
-
-$path = array($library, get_include_path());
-set_include_path(implode(PATH_SEPARATOR, $path));
-
-require_once 'Stormpath/Stormpath.php';
-
 // Include the composer autoloader
 require_once dirname(__DIR__) . '/vendor/autoload.php';
-
-include 'Stormpath/Tests/BaseTest.php';
-
-unset($root, $library, $path, $loader);
